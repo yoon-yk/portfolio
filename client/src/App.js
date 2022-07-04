8b62ca373621f9a3296db5bc74b3dd9b97a83a43
suejjangxx <youngk@u.sogang.ac.kr>
2022-07-04 17:27:56 +0900

    Fix npm error

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
-import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import './App.css'
 
